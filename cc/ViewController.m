3256e4d49356ff00e3f8454cd24df1808d5e3f09
iMac <imac@iMacdeMacBook-Air.local>
2017-08-23 00:10:57 +0800

    123

--- cc/ViewController.m
+++ cc/ViewController.m
@@ -19,6 +19,13 @@
     // Do any additional setup after loading the view, typically from a nib.
     
     
+    NSLog(@"123");
+    
+    NSLog(@"123");
+    
+    NSLog(@"123");
+    
+    NSLog(@"123");
     
     
     NSLog(@"123");
